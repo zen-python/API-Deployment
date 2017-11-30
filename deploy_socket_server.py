cad88e83aa8157a2d5c9ebac097a931669136ca9
Javier Torres Heredia <jtorres@live.cl>
2017-11-30 16:26:54 -0300

    dead prints of hell to stdin

--- deploy_socket_server.py
+++ deploy_socket_server.py
@@ -7,7 +7,6 @@ PORT = 54321
 
 
 def run_command(command):
-    print(command)
     p = subprocess.Popen(command,
                          shell=True, stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE,
@@ -22,7 +21,6 @@ while True:
         s.listen(1)
         conn, addr = s.accept()
         with conn:
-            print('Connected by', addr)
             while True:
                 data = conn.recv(1024)
                 if not data:
